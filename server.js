c0176e929a70d19f11a3496e39cc52c72e075774
lakhinsu <36450206+lakhinsu@users.noreply.github.com>
2018-03-11 13:41:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('aritcle',function(req,res){
+    res.send('Artcile one avse ahiya !');
+})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
