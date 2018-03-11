c3b278b41472c4febd54c23feca7fa3b18b531a4
lakhinsu <36450206+lakhinsu@users.noreply.github.com>
2018-03-11 15:08:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.send(path.join(__dirname,'ui','index.html'));
+  res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
 app.get('/article',function(req,res){
