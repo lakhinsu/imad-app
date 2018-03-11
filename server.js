0b42c08675e54a040c1a756a58205a9b3a5353d7
lakhinsu <36450206+lakhinsu@users.noreply.github.com>
2018-03-11 15:25:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-app.get(':/articlename',function(req,res){
+app.get('/:articlename',function(req,res){
     var articlename=req.params.aritclename;
     res.send(createTemplate(articles[articlename]));
 });
